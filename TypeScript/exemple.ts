cdcbe67d806a02fcdfa7ee9b599c4880bf06ceb9
ouailagzenay <ouailagzenay@gmail.com>
2022-02-17 10:50:56 +0100

    up

---
+++ TypeScript/exemple.ts
@@ -0,0 +1,11 @@
+// function greeter(name: string) {
+//     return "Bonjour, " + name;
+//  }
+
+//  var myName = "Sylvain Sidambarom";
+//  var ANumber = "ttt";
+  
+//  document.getElementById("display").innerHTML = greeter(ANumber);
+ var names: string[] = ['Pierre','Paul','Jaques'];
+
+ alert(names);
