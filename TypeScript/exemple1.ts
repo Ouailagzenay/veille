ec437d70161bb16b0f6fd2ce3bb932673ce5b937
ouailagzenay <ouailagzenay@gmail.com>
2022-02-23 15:57:50 +0100

    up

---
+++ TypeScript/exemple1.ts
@@ -0,0 +1,7 @@
+let message: string = 'Hello, TypeScript!';
+
+    let heading = document.createElement('h1');
+    heading.textContent = message;
+        
+    document.body.appendChild(heading);
+    
---
+++ TypeScript/exemple2.ts
@@ -0,0 +1,3 @@
+let skills: string[];
+skills = ['Problem Sovling','Software Design','Programming'];
+alert(skills[0]);
