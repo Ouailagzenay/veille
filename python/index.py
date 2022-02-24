d2e565782597bc4c2b97c94d9033fa0bfb0674e9
ouailagzenay <ouailagzenay@gmail.com>
2022-02-24 09:24:05 +0100

    UP

---
+++ python/index.py
@@ -0,0 +1,11 @@
+nom = input("entre le nom de l'étudiant ")
+note = int(input("entre le nom de l'étudiant"))
+
+if note <=18 :
+  print("tres bien")
+elif note <=14 :
+  print("bien")
+elif note <= 10 :
+  print("moyen") 
+else:
+  print("null")
